d56fb1e957bb6df8ef7cb6fe924647033db5e991
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2020-11-17 00:55:06 +0200

    Add heart display

--- Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -12,22 +12,63 @@ public class DestroyOnTrigger2D : MonoBehaviour {
     [SerializeField] int life;
 
     private void OnTriggerEnter2D(Collider2D other) {
-
-        if (life > 0 && other.tag == triggeringTag)
+        if (other.tag == HealTag)
         {
+            switch (life)
+            {
+                case 2: {
+                        GameObject.FindGameObjectWithTag("Heart3").GetComponent<Renderer>().enabled = true;
+                        GameObject.FindGameObjectWithTag("Heart2").GetComponent<Renderer>().enabled = true;
+                        GameObject.FindGameObjectWithTag("Heart1").GetComponent<Renderer>().enabled = true;
+                    }
+                    break;
+                case 1:
+                    {
+                        GameObject.FindGameObjectWithTag("Heart3").GetComponent<Renderer>().enabled = true;
+                        GameObject.FindGameObjectWithTag("Heart2").GetComponent<Renderer>().enabled = true;
+                    }break;
+				case 0:
+                    {
+                        GameObject.FindGameObjectWithTag("Heart3").GetComponent<Renderer>().enabled = true;
+                    }break;
+            }
+            if (life < 3)
+              life++;
+            Destroy(other.gameObject);
+        }
+        else if (life > 0 && other.tag == triggeringTag)
+        {
+            switch (life) {
+                case 3: {
+                        GameObject.FindGameObjectWithTag("Heart1").GetComponent<Renderer>().enabled = false;
+                    } break;
+                case 2:
+                    {
+                        GameObject.FindGameObjectWithTag("Heart1").GetComponent<Renderer>().enabled = false;
+                        GameObject.FindGameObjectWithTag("Heart2").GetComponent<Renderer>().enabled = false;
+                    }
+                    break;
+                case 1:
+                    {
+                        GameObject.FindGameObjectWithTag("Heart1").GetComponent<Renderer>().enabled = false;
+                        GameObject.FindGameObjectWithTag("Heart2").GetComponent<Renderer>().enabled = false;
+                        GameObject.FindGameObjectWithTag("Heart3").GetComponent<Renderer>().enabled = false;
+                    }
+                    break;
+
+            }
+            
             life--;
             Destroy(other.gameObject);
         }
-        else if (other.tag == triggeringTag && enabled && other.tag != "Player")
+
+        else if (other.tag == triggeringTag && enabled)
         {
+            
             Destroy(this.gameObject);
             Destroy(other.gameObject);
         }
-        else if (other.tag == HealTag)
-        {
-            life++;
-            Destroy(other.gameObject);
-        }
+
         
     }
 }
